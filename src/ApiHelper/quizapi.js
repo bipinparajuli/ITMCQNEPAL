70c2892dc3e5318e9a63433f1727a1161179772a
bipinparajuli <bipinprjl@gmail.com>
2021-05-25 18:37:26 +0545

    baisc read write done

--- src/ApiHelper/quizapi.js
+++ src/ApiHelper/quizapi.js
@@ -12,6 +12,26 @@ export const getAllQuestion = (subject)=> {
 }
 
 //post question to database
-export const postQuestion = ()=> {
-    
+export const postQuestion = (description,alternatives,category)=> {
+//  console.log(question,category,alternatives);
+    return fetch(`http://localhost:8000/api/createquestion`,{
+        method:"POST",
+    headers:{
+        Accept:'application/json',
+        "Content-Type":"application/json"
+    },
+        body:JSON.stringify({description,alternatives,category})
+    }).then(response=>
+        {
+            console.log(response);
+       return response.json()
+         
+        })
+    .catch(err=>
+        {
+            console.log(err);
+
+            return err
+        }
+        )
 }
--- src/Component/Admin/Admin.js
+++ src/Component/Admin/Admin.js
@@ -1,26 +1,68 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {TextField,Button,MenuItem} from '@material-ui/core'
+import {postQuestion} from "../../ApiHelper/quizapi"
+import {toast} from 'react-toastify';
 
 const Admin = () => {
+const [question, setquestion] = useState("");
+const [answer, setanswer] = useState({
+answerA:"",
+isCorrectA:false,
+answerB:"",
+isCorrectB:false,
+answerC:"",
+isCorrectC:false,
+answerD:"",
+isCorrectD:false,
+});
+const [subject, setsubject] = useState("");
+const [istrue, settrue] = useState(false);
 
-    return (
+const submitQuestion = () => {
+  postQuestion(question,answer,subject)
+  .then(data=>toast("Added Successfully",{type:"success"}))
+  .catch(e=>toast("Failed to add",{type:"error"}))
+}
+
+
+return (
         <div style={{width:"100%",height:"100vh",display:"flex",flexDirection:"column",justifyContent:'start',alignItems:"center"}}>
-  <TextField id="outlined-basic" label="Question" variant="outlined" /><br />
-  <TextField id="outlined-basic" label="Answer A" variant="outlined" /><br />
-  <TextField id="outlined-basic" label="Answer B" variant="outlined" /><br />
-  <TextField id="outlined-basic" label="Answer C" variant="outlined" /><br />
-  <TextField id="outlined-basic" label="Answer D" variant="outlined" /><br />
 
+  <TextField required id="outlined-basic" onChange={e=>setquestion(e.target.value)} label="Question" variant="outlined" /><br />
 
-  <select>
-    <option>Select</option>
-    <option>Yes</option>
-    <option>No</option>
+  <TextField required id="outlined-basic" onChange={e=>setsubject(e.target.value)} label="Subject" variant="outlined" /><br />
 
+  <TextField required id="outlined-basic" onChange={e=>setanswer({...answer,answerA:e.target.value})}  label="Answer A" variant="outlined" />
+  <select onChange={e=>setanswer({...answer,isCorrectA:e.target.value})}>
+    <option>Answer</option>
+    <option value="true">True</option>
+    <option value="false">Flase</option>
     </select>
-    <br />
+  <br />
+  <TextField required id="outlined-basic" onChange={e=>setanswer({...answer,answerB:e.target.value})} label="Answer B" variant="outlined" />
+  <select onChange={e=>setanswer({...answer,isCorrectB:e.target.value})}>
+    <option>Answer</option>
+    <option value="true">True</option>
+    <option value="false">Flase</option>
+    </select>
+  <br />
+  <TextField required id="outlined-basic" onChange={e=>setanswer({...answer,answerC:e.target.value})} label="Answer C" variant="outlined" />
+  <select onChange={e=>setanswer({...answer,isCorrectC:e.target.value})}>
+    <option>Answer</option>
+    <option value="true">True</option>
+    <option value="false">Flase</option>
+    </select>
+  <br />
+  <TextField required id="outlined-basic" onChange={e=>setanswer({...answer,answerD:e.target.value})} label="Answer D" variant="outlined" />
+  <select onChange={e=>setanswer({...answer,isCorrectD:e.target.value})}>
+    <option>Answer</option>
+    <option value="true">True</option>
+    <option value="false">Flase</option>
+    </select>
+  <br />
 
-  <Button variant="contained" color="primary">
+
+  <Button onClick={submitQuestion} variant="contained" color="primary">
   Submit
 </Button>
 
