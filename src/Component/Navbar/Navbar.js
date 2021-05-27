53ca7ecf690f1135e8d26d7935e28dc3f16927c7
bipinparajuli <bipinprjl@gmail.com>
2021-05-27 17:49:35 +0545

    deployiong

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,47 +1,73 @@
 import React from 'react'
-import "./Navbar.css"
 import {Link} from 'react-router-dom'
-
 const Navbar = () => {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-fluid">
-      <a class="navbar-brand" href="#">ITMCQNEPAL</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="main_nav">
-        <ul class="navbar-nav">
-          <li class="nav-item active"> <a class="nav-link" href="#">Home </a> </li>
-          <li class="nav-item"><a class="nav-link" href="#"> About </a></li>
-          <li class="nav-item dropdown" id="myDropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">  Treeview menu  </a>
-            <ul class="dropdown-menu">
-              <li> <a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
-              <li> <a class="dropdown-item" href="#"> Dropdown item 2 &raquo; </a>
-                <ul class="submenu dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Submenu item 1</a></li>
-                  <li><a class="dropdown-item" href="#">Submenu item 2</a></li>
-                  <li><a class="dropdown-item" href="#">Submenu item 3 &raquo; </a>
-                    <ul class="submenu dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Multi level 1</a></li>
-                      <li><a class="dropdown-item" href="#">Multi level 2</a></li>
-                    </ul>
-                  </li>
-                  <li><a class="dropdown-item" href="#">Submenu item 4</a></li>
-                  <li><a class="dropdown-item" href="#">Submenu item 5</a></li>
-                </ul>
-              </li>
-              <li><a class="dropdown-item" href="#"> Dropdown item 3 </a></li>
-              <li><a class="dropdown-item" href="#"> Dropdown item 4 </a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      {/* <!-- navbar-collapse.// --> */}
-      </div>
-      {/* <!-- container-fluid.// --> */}
-      </nav>    )
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a className="navbar-brand" href="#">ITMCQNEPAL</a>
+    
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+    
+    <li className="nav-item dropdown">
+       
+       {/* BCA */}
+
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLinkbca" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          bca
+        </a>
+       
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkbca">
+          
+                <Link className="nav-link dropdown-item dropdown-toggle"   id="navbarDropdownMenuLinkbca1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">    
+                    1st sem
+                </Link>
+          
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkbca1" >
+
+                        <Link className="dropdown-item" to="/question/C">CFA</Link>
+                        <Link className="dropdown-item" to="/question/math">Math</Link>
+                        <Link className="dropdown-item" to="/question/digitallogic">English</Link>
+                        <Link className="dropdown-item" to="/question/english">Sociology and technology</Link>
+                        <Link className="dropdown-item" to="/question/socialogy">Digital Logic</Link>
+                  
+                    </div>
+
+
+                <Link className="dropdown-item dropdown-toggle" to=""  id="navbarDropdownMenuLinkbca2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  2nd
+                </Link>
+                      
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkbca2" >
+
+                          <a className="dropdown-item" href="/question/C">C</a>
+                          <a className="dropdown-item" href="/question/math">Math</a>
+                          <a className="dropdown-item" href="/question/english">English</a>
+                          <a className="dropdown-item" href="/question/microprocessor">Microprocessor</a>
+                          <a className="dropdown-item" href="/question/account">Account</a>
+
+                    </div>
+
+
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">mba</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">csit</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+
+
+        </div>
+    )
 }
 
 export default Navbar
